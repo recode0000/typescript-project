8307c6d83868759e63560f0dea1f7b82ec409f5b
Rikito Ikeda <rikito1036@icloud.com>
2023-05-10 07:03:29 +0900

    データ型、intersecton Types、Union Types

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,59 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let userName = "hi";
+let dummyNum = 1122
+let bool: boolean = true;
+
+let array1 = [true, false, true];
+let array2 = [0, false, true];
+
+interface Name {
+  first: string;
+  last?: string | null;
+}
+
+let name: Name = {first: "yamada"};
+
+const func01 = (x: number, y: number): number => {
+  return x + y;
+}
+
+// Intersction Types
+type PROFILE = {
+  age: number;
+  city: string;
+};
+
+type LOGIN = {
+  username: string;
+  password: string;
+};
+
+type USER = PROFILE & LOGIN;
+
+const userA: USER = {
+  age: 30,
+  city: "Tokyo",
+  username: 'xxx',
+  password: "yyy"
+}
+
+
+// union Types
+let value: boolean | number
+value = true
+value = 100;
+
+let arrayUni: (number | string)[];
+arrayUni = [0, 1, 2, "hello"];
+
+
+let company: "Facebook" | "Google" | "Amazon"
+company = "Amazon";
+
+
+
 function App() {
   return (
     <div className="App">
