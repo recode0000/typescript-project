971f7abba5673fabbc5f0b62f5a53855e60e8bdc
Rikito Ikeda <rikito1036@icloud.com>
2023-05-10 07:14:36 +0900

    typeof keyof enum

--- src/App.tsx
+++ src/App.tsx
@@ -54,6 +54,59 @@ let company: "Facebook" | "Google" | "Amazon"
 company = "Amazon";
 
 
+//typeof
+let msg: string = "Hi";
+let msg2: typeof msg;
+
+let animal = {cat: "small cat"};
+let newAnimal: typeof animal = {cat: "big cat"};
+
+//keyof
+type KEYS = {
+  primary: string;
+  secondary: string;
+};
+
+let key: keyof KEYS;
+
+//typeof + keyof
+const SPORTS = {
+  soccer: "Soccer",
+  baseball: "Baseball",
+}
+
+let keySports: keyof typeof SPORTS;
+
+
+//enum
+enum OS {
+  Windows,
+  Mac,
+  Linux,
+}
+
+interface PC {
+  id: number;
+  OSType: OS;
+}
+
+const PC1: PC = {
+  id: 1,
+  OSType: OS.Windows
+}
+
+const PC2: PC = {
+  id: 2,
+  OSType: OS.Mac
+}
+
+const PC3: PC = {
+  id: 3,
+  OSType: OS.Linux
+}
+
+
+
 
 function App() {
   return (
